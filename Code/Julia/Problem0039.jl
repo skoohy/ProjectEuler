b35f0ed1497a4aaf5e8b31c936ba2d95cc16f8dc
Shawn Koohy <103887212+skoohy@users.noreply.github.com>
2022-10-10 16:53:30 -0400

    Add files via upload

---
+++ Code/Julia/Problem0039.jl
@@ -0,0 +1,19 @@
+using StatsBase
+
+function euler39()
+    perimeters = []; p = 1000
+
+    for a in range(1, 500)
+        for b in range(a, 500)
+            c = sqrt(a^2 + b^2)
+                if round(c) == c || a + b + c <= p
+                    push!(perimeters, a+b+c)
+                end
+        end
+    end
+    return mode(perimeters)
+end
+@time begin
+euler39()
+end
+print(euler39())
