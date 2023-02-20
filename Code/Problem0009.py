79c3670050c641a1ffbb6f6e18ad1b4bf8dd690f
Shawn Koohy <103887212+skoohy@users.noreply.github.com>
2023-02-20 08:54:35 -0500

    Delete Problem0009.py

--- Code/Problem0009.py
+++
@@ -1,9 +0,0 @@
-import time
-t1 = time.time()
-
-for a in range(1, 500):
-    for b in range(a+1, 500):
-        c = 1000 - b - a
-        if a**2 + b**2 == c**2:
-            print(a*b*c)
-            print(f'Program Execution Time: {time.time()-t1} seconds')
