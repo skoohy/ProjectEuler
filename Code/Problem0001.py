00f47fc923337a20271fc2c9ff134876e43f9467
Shawn Koohy <103887212+skoohy@users.noreply.github.com>
2023-02-20 08:40:11 -0500

    Update Problem0001.py

--- Code/Problem0001.py
+++ Code/Problem0001.py
@@ -1,15 +1,10 @@
-import time
-start_time = time.time()
+N = 1000
 
+threes   = range(3, N, 3)
+fives    = range(5, N, 5)
+fifteens = range(15, N, 15)
 
-def sum_of_multiples(N):
-    threes   = range(3, N, 3)
-    fives    = range(5, N, 5)
-    fifteens = range(15, N, 15)
-    return sum(threes) + sum(fives) - sum(fifteens)
+ans = sum(threes) + sum(fives) - sum(fifteens)
 
-print(sum_of_multiples(1000))
+print(ans)
 
-
-end_time = time.time()
-print(f'Program Execution Time: {end_time-start_time} seconds')
